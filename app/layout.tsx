c8dd8f83c3ffce60172b376b335e3cb135db4f83
fixonz <109214021+fixonz@users.noreply.github.com>
2025-08-06 10:20:03 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'RRA NEWS AGGREGATOR',
-  description: 'UnderDev | RRA NEWS AGGREGATOR',
+  title: 'CRYPTO NEWS AGGREGATOR',
+  description: 'Nickys | CRYPTO NEWS AGGREGATOR',
   generator: '',
   icons: {
     icon: '/placeholder-logo.png',
